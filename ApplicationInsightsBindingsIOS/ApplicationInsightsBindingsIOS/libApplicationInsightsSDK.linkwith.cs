dee60c5e3e66286dc99807c0b88d4a0d5cb27582
Christoph Wendt <chwend@microsoft.com>
2015-06-03 16:25:19 -0700

    Update Application Insights iOS SDK static lib

--- ApplicationInsightsBindingsIOS/ApplicationInsightsBindingsIOS/libApplicationInsightsSDK.linkwith.cs
+++ ApplicationInsightsBindingsIOS/ApplicationInsightsBindingsIOS/libApplicationInsightsSDK.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith ("libApplicationInsightsSDK.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64, SmartLink = true, ForceLoad = true, Frameworks="CoreTelephony, SystemConfiguration")]
+[assembly: LinkWith ("libApplicationInsightsSDK.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64, SmartLink = true, ForceLoad = true, Frameworks = "CoreTelephony SystemConfiguration Security")]
