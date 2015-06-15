a753a7fac6941277da0390c5adcf8dac2f236300
Christoph Wendt <chwend@microsoft.com>
2015-06-15 14:25:36 -0700

    Add methods to wrapper classes

--- XamarinTest/ApplicationInsights.cs
+++ XamarinTest/ApplicationInsights.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Android.App;
+using Android.Content;
 using Xamarin.Forms;
 
 namespace XamarinTest
@@ -11,8 +13,64 @@ namespace XamarinTest
 			DependencyService.Get<IApplicationInsights> ().Setup (instrumentationKey);
 		}
 
+		public static void Setup (Context context, Android.App.Application application, string instrumentationKey){
+			DependencyService.Get<IApplicationInsights> ().Setup (context, application, instrumentationKey);
+		}
+
 		public static void Start (){
 			DependencyService.Get<IApplicationInsights> ().Start ();
+		}
+
+		public static string GetServerUrl (){
+			DependencyService.Get<IApplicationInsights> ().GetServerUrl ();
+		}
+
+		public static void SetServerUrl (string serverUrl){
+			DependencyService.Get<IApplicationInsights> ().SetServerUrl (serverUrl);
+		}
+
+		public static void SetCrashManagerDisabled (bool crashManagerDisabled){
+			DependencyService.Get<IApplicationInsights> ().SetCrashManagerDisabled (crashManagerDisabled);
+		}
+
+		public static void SetTelemetryManagerDisabled (bool telemetryManagerDisabled){
+			DependencyService.Get<IApplicationInsights> ().SetTelemetryManagerDisabled (telemetryManagerDisabled);
+		}
+
+		public static void SetAutoPageViewTrackingDisabled (bool autoPageViewTrackingDisabled){
+			DependencyService.Get<IApplicationInsights> ().SetAutoPageViewTrackingDisabled  (autoPageViewTrackingDisabled);
+		}
+
+		public static void SetAutoSessionManagementDisabled (bool autoSessionManagementDisabled){
+			DependencyService.Get<IApplicationInsights> ().SetAutoSessionManagementDisabled  (autoSessionManagementDisabled);
+		}
+
+		public static void SetUserId (string userId){
+			DependencyService.Get<IApplicationInsights> ().SetUserId  (userId);
+		}
+
+		public static void StartNewSession (){
+			DependencyService.Get<IApplicationInsights> ().StartNewSession  ();
+		}
+
+		public static void SetSessionExpirationTime (int appBackgroundTime){
+			DependencyService.Get<IApplicationInsights> ().SetSessionExpirationTime (appBackgroundTime);
+		}
+
+		public static void RenewSessionWithId (string sessionId){
+			DependencyService.Get<IApplicationInsights> ().RenewSessionWithId (sessionId);
+		}
+
+		public static bool GetAppStoreEnvironment(){
+			DependencyService.Get<IApplicationInsights> ().GetAppStoreEnvironment ();
+		}
+
+		public static bool GetDebugLogEnabled(){
+			DependencyService.Get<IApplicationInsights> ().GetDebugLogEnabled ();
+		}
+
+		public static void SetDebugLogEnabled(bool debugLogEnabled){
+			DependencyService.Get<IApplicationInsights> ().SetDebugLogEnabled (debugLogEnabled);
 		}
 	}
 }
--- XamarinTest/TelemetryManager.cs
+++ XamarinTest/TelemetryManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Xamarin.Forms;
+using System.Collections.Generic;
 
 namespace XamarinTest
 {
@@ -11,6 +12,45 @@ namespace XamarinTest
 		{
 			DependencyService.Get<ITelemetryManager>().TrackEvent(eventName);
 		}
+
+		public static void TrackEvent (string eventName, Dictionary<string, string> properties){
+			DependencyService.Get<ITelemetryManager>().TrackEvent(eventName, properties);
+		}
+
+		public static void TrackEvent (string eventName, Dictionary<string, string> properties, Dictionary<string, double> measurements){
+			DependencyService.Get<ITelemetryManager>().TrackEvent(eventName, properties, measurements);
+		}
+
+		public static void TrackTrace (string message){
+			DependencyService.Get<ITelemetryManager>().TrackTrace(message);
+		}
+
+		public static void TrackTrace (string message, Dictionary<string, string> properties){
+			DependencyService.Get<ITelemetryManager>().TrackTrace(message, properties);
+		}
+
+		public static void TrackMetric (string metricName, double value){
+			DependencyService.Get<ITelemetryManager>().TrackMetric(metricName, value);
+		}
+
+		public static void TrackMetric (string metricName, double value, Dictionary<string, string> properties){
+			DependencyService.Get<ITelemetryManager>().TrackMetric(metricName, value, properties);
+		}
+
+		public static void TrackPageView (string pageName){
+			DependencyService.Get<ITelemetryManager>().TrackPageView(pageName);
+		}
+
+		public static void TrackPageView (string pageName, int duration){
+			DependencyService.Get<ITelemetryManager>().TrackPageView(pageName, duration);
+		}
+
+		public static void TrackPageView (string pageName, int duration, Dictionary<string, string> properties){
+			DependencyService.Get<ITelemetryManager>().TrackPageView(pageName, duration, properties);
+		}
+
+		public static void TrackManagedException (Exception  exception, bool handled){
+			DependencyService.Get<ITelemetryManager>().TrackManagedException(exception, handled);
+		}
 	}
 }
-
