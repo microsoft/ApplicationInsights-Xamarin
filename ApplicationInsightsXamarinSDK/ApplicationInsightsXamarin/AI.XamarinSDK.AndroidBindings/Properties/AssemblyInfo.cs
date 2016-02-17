306dac39e3621507f9bdb6ca8856d65e310c67d9
Christoph Wendt <chwend@microsoft.com>
2016-02-17 14:10:46 -0800

    Set NSAllowsArbitraryLoads for iOS demo project

--- ApplicationInsightsXamarinSDK/ApplicationInsightsXamarin/AI.XamarinSDK.AndroidBindings/Properties/AssemblyInfo.cs
+++ ApplicationInsightsXamarinSDK/ApplicationInsightsXamarin/AI.XamarinSDK.AndroidBindings/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using Android.App;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("christophwendt")]
+[assembly: AssemblyCopyright ("Microsoft")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
