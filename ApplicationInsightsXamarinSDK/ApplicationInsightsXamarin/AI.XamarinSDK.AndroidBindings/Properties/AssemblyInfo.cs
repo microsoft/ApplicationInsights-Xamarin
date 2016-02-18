38e904c6a52d8bf4835e6379e61d0223d113e744
Christoph Wendt <chwend@microsoft.com>
2016-02-18 09:56:36 -0800

    Change copy right

--- ApplicationInsightsXamarinSDK/ApplicationInsightsXamarin/AI.XamarinSDK.AndroidBindings/Properties/AssemblyInfo.cs
+++ ApplicationInsightsXamarinSDK/ApplicationInsightsXamarin/AI.XamarinSDK.AndroidBindings/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using Android.App;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("Microsoft")]
+[assembly: AssemblyCopyright ("christophwendt")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
